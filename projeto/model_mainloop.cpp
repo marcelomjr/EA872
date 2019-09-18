9ff9df2d1b0ac7fa5136d5e4db4d0cc8adc6de43
marcelomjr <martimiano.marcelo@gmail.com>
2019-09-17 22:56:10 -0300

    Bip funcionando pelo teclado

--- projeto/model_mainloop.cpp
+++ projeto/model_mainloop.cpp
@@ -12,67 +12,60 @@
 #include "01-playback.hpp"
 
 using namespace std::chrono;
+
+volatile int audio_thread_running = 0;
+Audio::Sample *asample;
+Audio::Player *player;
+
+
 uint64_t get_now_ms() {
   return duration_cast<milliseconds>(steady_clock::now().time_since_epoch()).count();
 }
 
-void delay(uint64_t ms) {
+void* audio_thread_handler(void* params) {
   uint64_t t0;
   uint64_t t1;
+  if (audio_thread_running == 1) {
+  	return NULL;
+  }
+  
+  player->play(asample);
   
   while (1) {
    std::this_thread::sleep_for (std::chrono::milliseconds(1));
    t1 = get_now_ms();
-   if (t1-t0 > ms) break;
+   if (t1-t0 > 500) break;
   }
+  asample->set_position(0);
+  
+  audio_thread_running = 0;
+  
+  return NULL;
 }
 
 int main ()
 {
+  uint64_t t2, t3;
   uint64_t t0;
   uint64_t t1;
   uint64_t deltaT;
   uint64_t T = get_now_ms();
+  pthread_t audio_thread;
 
-  Audio::Sample *asample;
   asample = new Audio::Sample();
   asample->load("assets/blip.dat");
 
-  Audio::Player *player;
   player = new Audio::Player();
   player->init();	
   
   while (1) {
     std::this_thread::sleep_for (std::chrono::milliseconds(1));
     t1 = get_now_ms();
-    if (t1-t0 > 500) break;
+    if (t1-t0 > 1000) break;
   }
 
-  std::cout << "Audio initialized!\n"; 
-  
-  player->play(asample);
-
-  t0 = get_now_ms();
-  while (1) {
-    std::this_thread::sleep_for (std::chrono::milliseconds(1));
-    t1 = get_now_ms();
-
-    if (t1-t0 > 500) break;
-  }
-
-  asample->set_position(0);
-
-  t0 = get_now_ms();
-  while (1) {
-    std::this_thread::sleep_for (std::chrono::milliseconds(1));
-    t1 = get_now_ms();
-
-    if (t1-t0 > 500) break;
-  }
-
-
-  player->stop();
-	
+  std::cout << "Audio initialized!\n";
+  	
   // massa, velocidade, posicao, const_elastica, const_amortecimento
   Corpo *c1 = new Corpo(1, 0, 180, 4, 0.8);
   Corpo *c2 = new Corpo(1, 0, 180, 4, 1.0);
@@ -88,7 +81,7 @@ int main ()
   Fisica *f = new Fisica(l);
 
   Tela *tela = new Tela(l, 20, 100, 200, 200);
-  //tela->init();
+  tela->init();
   
   Teclado *teclado = new Teclado();
   teclado->init();
@@ -110,8 +103,17 @@ int main ()
     char c = teclado->getchar();
     if (c=='d') {
       f->choque(1);
+      
+      if (audio_thread_running != 1) {
+      	// Toca som a teclar "d"
+      	pthread_create(&audio_thread, NULL, audio_thread_handler, NULL); 
+      }
     }
     else if (c=='a') {
+	   if (audio_thread_running != 1) {
+         // Toca som a teclar "a"
+         pthread_create(&audio_thread, NULL, audio_thread_handler, NULL);
+      }
       f->choque(-1);
     }
     else if (c=='q') {
@@ -122,5 +124,9 @@ int main ()
 
   }
   tela->stop();
+  pthread_join(audio_thread, NULL);
+  player->stop();
+
+  
   return 0;
 }
